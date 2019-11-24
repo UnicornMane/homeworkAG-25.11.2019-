d95dccc72ab3d99fc6ec5a196dc21ecd30a04396
Alexandr Isaenkov <48714228+UnicornMane@users.noreply.github.com>
2019-11-25 00:16:39 +0300

    Add files via upload

---
+++ 4.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+	long long n, k;
+	cin >> n >> k;
+	
+	vector <long long> arr(n);
+	
+	for (auto i = 0; i < n; i++)
+	{
+		cin >> arr[i];
+	}
+
+	for (auto i = 0; i < n; i++)
+	{
+		auto t = arr[i];
+		auto tmpK = 1;
+		for (auto j = 0; t; j++)
+		{
+			if (t % 10 != 0)
+			{
+				tmpK *= t % 10;
+			}
+			t /= 10;
+		}
+		if (abs(tmpK) > k)
+		{
+			if (arr[i] == 0 && arr[i] > k)
+			{
+				cout << arr[i] << " ";
+			}
+			else if (arr[i] != 0)
+			{
+				cout << arr[i] << " ";
+			}
+		}
+	}
+}
+/*
+ * перебор эффективный, т.к. работает за O(n * log n)
+ */
