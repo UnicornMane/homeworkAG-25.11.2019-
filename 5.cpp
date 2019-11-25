22c55518f21a2e79661c3f5c402c57714a9283ca
Alexandr Isaenkov <48714228+UnicornMane@users.noreply.github.com>
2019-11-26 00:30:00 +0300

    Add files via upload

---
+++ 5.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void erSieving(int &n)
+{
+	vector <int> f(n + 3, 0);
+	int d = 2;
+	while (d * d <= n)
+	{
+		for (int i = d * d; i <= n; i += d)
+		{
+			f[i]++;
+		}
+		++d;
+	}
+	for (int i = 2; i <=n; i++)
+	{
+		if (f[i + 2] <= 1 && f[i] == 0)
+		{
+			cout << i << " ";
+		}
+	}
+}
+
+int main()
+{
+	int n;
+	cin >> n;
+
+	erSieving(n);
+}
